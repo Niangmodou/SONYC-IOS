2226be0f4d87fb8a533911affd10b16b07af12f0
Modou <niangmodou100@gmail.com>
2020-06-10 17:41:35 -0500

    Recording sound functionality complete

--- SONYC Initial Project/SONYC Initial Project/SONYC Initial Project/ViewController.swift
+++ SONYC Initial Project/SONYC Initial Project/SONYC Initial Project/ViewController.swift
@@ -15,6 +15,46 @@ class ViewController : UIViewController, AVAudioRecorderDelegate {
     var recordingSession: AVAudioSession!
     var audioRecorder: AVAudioRecorder!
     
+    //Variable to track number of current recordings
+    var numRecords : Int = 0
+    
+    //Outlet for Button
+    @IBOutlet weak var button: UIButton!
+    
+    //Action for record button
+    @IBAction func record(_ sender: Any) {
+        //Check for active recording
+        if audioRecorder == nil {
+            numRecords += 1
+            
+            let fileName = getPathDirectory().appendingPathComponent("\(numRecords).m4a")
+            
+            //Define settings for current recording
+            let settings = [AVFormatIDKey: Int(kAudioFormatMPEG4AAC), AVSampleRateKey: 12000, AVNumberOfChannelsKey: 1, AVEncoderAudioQualityKey: AVAudioQuality.high.rawValue]
+            
+            //Start Audio recording
+            do {
+                audioRecorder = try AVAudioRecorder(url: fileName, settings: settings)
+                audioRecorder.delegate = self
+                
+                audioRecorder.record(forDuration: 10)
+                
+                button.setTitle("Stop Recording", for: .normal)
+                button.setTitleColor(UIColor.red, for: .normal)
+            }catch {
+                displayAlert(title: "Error", message: "Recording Failed")
+            }
+        }else{
+            //Stop Recording
+            audioRecorder.stop()
+            audioRecorder = nil
+            
+            //Saving number of recording to user defaults
+            UserDefaults.standard.set(numRecords, forKey: "myNumber")
+            button.setTitle("Record Sound", for: .normal)
+            button.setTitleColor(UIColor.blue, for: .normal)
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,6 +62,9 @@ class ViewController : UIViewController, AVAudioRecorderDelegate {
         //Setting Up Session
         recordingSession = AVAudioSession.sharedInstance()
         
+        if let number : Int = UserDefaults.standard.object(forKey: "myNumber") as? Int {
+            numRecords = number
+        }
         // Ask user for permission to use microphone
         AVAudioSession.sharedInstance().requestRecordPermission{ (hasPermission) in
             if hasPermission {
@@ -32,8 +75,20 @@ class ViewController : UIViewController, AVAudioRecorderDelegate {
         //UI Button for user to record
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
+    //Gets path to directory
+    func getPathDirectory() -> URL {
+        //Searches a FileManager for paths and returns the first one
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        let documentDirectory = paths[0]
+        
+        return documentDirectory
+    }
+    
+    //Displays alert
+    func displayAlert(title:String, message: String){
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "dismiss", style: .default, handler: nil))
+        present(alert, animated:true, completion: nil)
     }
 }
 
